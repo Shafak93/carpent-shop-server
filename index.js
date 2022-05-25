01cebc29614046cdadd2b0fa0dcbe61e3079dce9
Shafak93 <shafakctg64@gmail.com>
2022-05-25 14:39:31 +0600

    require jwt and  made a ACCESS_TOKEN

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
