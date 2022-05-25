0ce4bd2696b78c5ec244c758c0c0ac732e4d7c8d
Shafak93 <shafakctg64@gmail.com>
2022-05-25 14:31:36 +0600

    json web token install

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ async function run(){
             res.send(products);
         })
         //PUT API for user
-        app.put('/user/:email', async(req, res){
+        app.put('/user/:email', async(req, res)=>{
             const email = req.params.email;
             const user = req.body;
             const filter = {email : email};
